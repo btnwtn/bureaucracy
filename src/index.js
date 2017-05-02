3b2099b0f000f7eca621a8d4252773cd56e4e8e8
btnwtn <btnwtn@gmail.com>
2017-05-02 12:25:39 -0700

    Modularize!
    Setup this repo to be NPM module. Add basic type checking that will
    allow for rules to be Promises or regular arrays.

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const rejectFalsey = collection => {
+  if (typeof collection === 'undefined') return []
+
+  if (Array.isArray(collection)) {
+    return collection.filter(v => v)
+  } else {
+    return [collection].filter(v => v)
+  }
+}
+
+const createValidator = rules => field => value => {
+  return new Promise(resolve => {
+    const validator = rules[field];
+
+    if (!validator) {
+      throw new Error(`Missing validator for field: ${JSON.stringify(field)}`);
+    }
+
+    const result = validator(value)
+
+    if (typeof result === 'undefined') {
+      throw new Error(`Rule for ${JSON.stringify(field)} returned undefined.`)
+    }
+
+    if (typeof result.then === 'function') {
+      return result.then(errors => {
+        resolve(rejectFalsey(errors))
+      })
+    }
+
+    return resolve(rejectFalsey(errors))
+  })
+};
+
+export default createValidator
