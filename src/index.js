f01dd3b0cc51a17c4833218d4c1d10178962e792
btnwtn <btnwtn@gmail.com>
2017-05-02 12:29:15 -0700

    Run prettier manually

--- src/index.js
+++ src/index.js
@@ -1,35 +1,41 @@
 const rejectFalsey = collection => {
-  if (typeof collection === 'undefined') return []
+  if (typeof collection === "undefined") return [];
 
   if (Array.isArray(collection)) {
-    return collection.filter(v => v)
+    return collection.filter(v => v);
   } else {
-    return [collection].filter(v => v)
+    return [collection].filter(v => v);
   }
-}
-
-const createValidator = rules => field => value => {
-  return new Promise(resolve => {
-    const validator = rules[field];
-
-    if (!validator) {
-      throw new Error(`Missing validator for field: ${JSON.stringify(field)}`);
-    }
-
-    const result = validator(value)
-
-    if (typeof result === 'undefined') {
-      throw new Error(`Rule for ${JSON.stringify(field)} returned undefined.`)
-    }
-
-    if (typeof result.then === 'function') {
-      return result.then(errors => {
-        resolve(rejectFalsey(errors))
-      })
-    }
-
-    return resolve(rejectFalsey(errors))
-  })
 };
 
-export default createValidator
+const createValidator = rules =>
+  field =>
+    value => {
+      return new Promise(resolve => {
+        const validator = rules[field];
+
+        if (!validator) {
+          throw new Error(
+            `Missing validator for field: ${JSON.stringify(field)}`
+          );
+        }
+
+        const result = validator(value);
+
+        if (typeof result === "undefined") {
+          throw new Error(
+            `Rule for ${JSON.stringify(field)} returned undefined.`
+          );
+        }
+
+        if (typeof result.then === "function") {
+          return result.then(errors => {
+            resolve(rejectFalsey(errors));
+          });
+        }
+
+        return resolve(rejectFalsey(errors));
+      });
+    };
+
+export default createValidator;
